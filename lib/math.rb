c7baf690cada6412ee51dbba213788e42ea491c0
Molly McCarron <mccarronmollye@gmail.com>
2018-10-13 00:37:05 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,15 +7,19 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
+  50/2
 end
 
 def multiplication(num1, num2)
+  4*30
 end
 
 def modulo(num1, num2)
+  34%5
 end
 
 def square_root(num)
+  Math.sqrt(81)
 end
 
 def order_of_operation(num1, num2, num3, num4)
